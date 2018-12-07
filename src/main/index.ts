3f2eba08f19f39cdf950d42966f4604ed8839df6
scliftas <scliftas@gmail.com>
2018-12-07 15:07:29 +0000

    Hidden title bar in browser window config

--- src/main/index.ts
+++ src/main/index.ts
@@ -13,7 +13,8 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    titleBarStyle: 'hidden'
   });
 
   mainWindow.loadURL(winURL);
