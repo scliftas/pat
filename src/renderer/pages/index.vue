7768f3db2c5e667ef3d7b776102f70ca94d221c0
scliftas <scliftas@gmail.com>
2018-12-07 14:43:20 +0000

    Initial commit

---
+++ src/renderer/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>{{ HelloWorld }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    data () {
+        return {
+            HelloWorld: 'Hello World 3'
+        }
+    }
+})
+</script>
